8d8bfdcd61095ad36a23a2372c8a3721bf0ac768
ItsKjaja <contact.kjaj@gmail.com>
2023-06-22 16:27:10 -0700

    Push Descr

--- 0-stack.c
+++ 0-stack.c
@@ -1,5 +1,10 @@
 #include "monty.h"
 
+/**
+ * push - Pushes an element to the stack.
+ * @stack: Double pointer to the stack.
+ * @line_number: Line number in the file.
+ */
 
 void push(stack_t **stack, unsigned int line_number)
 {
