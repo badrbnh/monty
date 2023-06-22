81340a1357821f553c00d9146de81d19f616eec1
badrbnh <badrechahide@gmail.com>
2023-06-22 15:13:22 +0100

    bug fixed 0

--- 0-stack.c
+++ 0-stack.c
@@ -45,11 +45,12 @@ void pall(stack_t **stack, unsigned int line_number)
 {
 	stack_t *current = *stack;
 
-	if (*stack == NULL)
+	if (current == NULL)
 	{
 		fprintf(stderr, "L%d: can't pall an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
+
 	while (current != NULL)
 	{
 		printf("%d\n", current->n);
