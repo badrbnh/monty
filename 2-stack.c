63434462d313efa1f8ccac5c3de288021eea218b
ItsKjaja <contact.kjaj@gmail.com>
2023-06-22 19:17:22 -0700

    changes making

--- 2-stack.c
+++ 2-stack.c
@@ -1,10 +1,6 @@
 #include "monty.h"
 
-/**
- * _mul - Multiplies the second top element of the stack with the top element.
- * @stack: Double pointer to the stack.
- * @line_number: Line number in the file.
- */
+
 void _mul(stack_t **stack, unsigned int line_number)
 {
 	stack_t *top, *next;
